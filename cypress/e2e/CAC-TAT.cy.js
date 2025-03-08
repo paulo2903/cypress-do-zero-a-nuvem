653d35cac60705cd942b756b0ebde03e4ec09753
Paulo Henrique <pauloh.2903@gmail.com>
2025-03-08 11:11:01 -0300

    Implementação do comando otimizado.

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,112 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {  //antes do início de cada teste, esse bloco será executado
+    cy.visit('./src/index.html')  //caminho da página local do projeto
+  })
+
+  it('verifica o título da aplicação', () => {    
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', () =>{   //o "only" serve para indicar que queremos que execute "somente" aquele teste específico
+    cy.get('#firstName').type('Paulo Henrique')  //quando tem # é porque o elemento é um id
+    cy.get('#lastName').type('Oliveira da Cruz')
+    cy.get('#email').type('pauloh.2903@gmail.com')
+    cy.get('#open-text-area').type('Obrigado pela MANUELLA!!!')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+
+
+
+it('preenche os campos para treinar', () =>{
+  const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)  //repeat -> para repetir quantas vezes indicarmos. nesse caso 10
+
+  cy.get('#firstName').type('Manuella França')
+  cy.get('#lastName').type('Cruz')
+  cy.get('#email').type('manuella@gmail.com')
+  cy.get('#open-text-area').type(longText, { delay:0 })  //parâmetro para tirar os 10 milisegundos de digitação default
+  cy.get('button[type="submit"]').click()
+
+  cy.get('.success').should('be.visible')
+  })
+
+
+it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () =>{
+  cy.get('#firstName').type('Manuella França')
+  cy.get('#lastName').type('Cruz')
+  cy.get('#email').type('manuella.gmail.zzz')  //e-mail inválido
+  cy.get('#open-text-area').type('Princesinha linda do papai !!!')
+  cy.get('button[type="submit"]').click()
+
+  cy.get('.error').should('be.visible')
+  })  
+
+
+  it('campo telefone continua vazio quando preenchido com um valor não-numérico', () =>{
+    cy.get('#phone')
+      .type('aaaaaaaa')
+      .should('have.value', '') //verifica se o campo ficou vazio como mostra nas aspas simples. 
+  })
+
+  
+  /* Exercício extra 5
+Uma funcionalidade que pode ser usada em conjunto com comando o .type(), é o .clear(), o qual limpa um campo, para posterior digitação, por exemplo.
+Portanto, crie um teste chamado preenche e limpa os campos nome, sobrenome, email e telefone
+Tal teste deve verificar o valor (value) após a digitação (.type(...).should('have.value', 'valor-aqui')) e após a limpeza do campo (.clear().should('have.value', ''))*/
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () =>{
+    cy.get('#firstName')
+      .type('Manuella França')
+      .should('have.value', 'Manuella França')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('Cruz')
+      .should('have.value', 'Cruz')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('manuella@gmail.com')      
+      .should('have.value', 'manuella@gmail.com')
+      .clear()
+      .should('have.value', '') 
+    cy.get('#phone')
+      .type('123456789')
+      .should('have.value', '123456789')
+      .clear()
+      .should('have.value', '') 
+    
+    cy.get('button[type="submit"]').click()    
+    }) 
+
+    /*Exercício extra 6
+      Crie um novo teste chamado exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios.
+      O teste deve simplesmente acessar a aplicação e clicar no botão Enviar
+      Tal teste deve verificar que uma mensagem é exibida em um elemento com a classe error*/
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () =>{       
+      cy.get('button[type="submit"]').click()   
+      cy.get('.error').should('be.visible') 
+      }) 
+
+
+
+  /* Exercício extra 7 - "Comandos customizados"
+No mundo de testes automatizados com Cypress, podemos fazer uso de comandos customizados para evitar duplicação de código.
+Criar um teste chamado envia o formuário com sucesso usando um comando customizado
+Tal teste deve fazer uso de um comando chamado fillMandatoryFieldsAndSubmit, o qual deve ser implementado no arquivo cypress/support/commands.js
+👨‍🏫 Sugiro que você experimente diferentes implementações para o mesmo comando (por exemplo, um comando que não recebe nenhum argumento; um comando que recebe um objeto como argumento; um comando que recebe um objeto como argumento, com valores padrão).
+Deve haver a verificação de que a mensagem de sucesso é exibida*/
+it('envia o formuário com sucesso usando um comando customizado', () => { 
+  const data = {
+    firstName: 'Paulo Henrique',
+    lastName: 'Oliveira da Cruz',
+    email: 'paulohenrique@gmail.com',
+    text: 'Teste.'
+  }  
+
+  cy.fillMandatoryFieldsAndSubmit(data)
+  
+  cy.get('.sucess').should('be.visible')   
+  }) 
+
+})
